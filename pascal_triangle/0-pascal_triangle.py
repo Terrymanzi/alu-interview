b7ce7a64b6254eaf919d068b8ecebcca54217c68
terrymanzi <m.terry@alustudent.com>
2024-10-14 18:42:28 +0300

    pascal triangle completed!

---
+++ pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    #first row
+    triangle = [[1]]
+    # start from the second row
+    i = 1  
+    
+    while i < n:
+        #every row should start with 1
+        row = [1]  
+        j = 1
+        while j < i:
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+            j += 1
+        row.append(1)
+        triangle.append(row)
+        i += 1
+    
+    return triangle
